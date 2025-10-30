a579c09c0ee044066691d28645335c5155e20a34
chiragshrimal <chirag1711shrimal@gmail.com>
2025-10-30 13:51:45 +0530

    correct index.ts file

--- index.ts
+++ index.ts
@@ -1,96 +1,69 @@
 import express from "express";
 import cors from "cors";
-
 const app= express();
+
+// install the dependency of the swaggerUi 
+import swaggerUi from 'swagger-ui-express'
+import fs from "fs"
+// we have writtern spec file in the yml 
+// so install yml also required
+import YAML from "yaml"
+
+// real the file and parse the file 
+const file  = fs.readFileSync('./spec.yml', 'utf8')
+const swaggerDocument = YAML.parse(file)
+
+// this end point give the swaggerUi 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(cors());
 
-// signup end point 
-app.post("/signup", (req,res)=>{
-    
-    const {email, password}= req.body();
-    
-    if(!email || !password){
-        return res.send({
-            message : "please provide the all information"
-        })
-    }
-    res.send({
-        message : "you have signup "
-    })
+// signup - POST /signup
+app.post("/signup", (req, res) => {
+  const { email, password } = req.body ?? {};
+  if (!email || !password) {
+    return res.status(400).json({ message: "please provide all information" });
+  }
+  return res.status(201).json({ message: "you have signed up" });
 });
 
-// signin end point 
-app.get("/signin",(req,res)=>{
-    const {email, password}= req.body();
-
-    if(!email || !password){
-        return res.send({
-            message : "please provide the all the information"
-        })
-    }
-
-    res.send({
-        token:"jwt_token"
-    })
-
+// signin 
+app.post("/signin", (req, res) => {
+  const { email, password } = req.body ?? {};
+  if (!email || !password) {
+    return res.status(400).json({ message: "please provide all information" });
+  }
+  return res.status(200).json({ token: "jwt_token" });
 });
 
-// helth check 
-app.get("/health", (req,res)=>{
-    res.send({
-        message : "i am working fine"
-    })
-})
-
-// create the todo
-app.post("/createTodo",(req,res)=>{
-    
-    const {title, description}= req.body();
-
-    if(!title || !description){
-        return res.send({
-            message : "please provide all the information"
-        })
-    }
-
-    res.send({
-        todo: [
-            {
-                id : "todo_id",
-                status : "done"
-            }
-        ]
-    });
+// health check
+app.get("/health", (_req, res) => {
+  return res.status(200).json({ message: "i am working fine" });
 });
 
+// create todo
+app.post("/createTodo", (req, res) => {
+  const { title, description } = req.body ?? {};
+  if (!title || !description) {
+    return res.status(400).json({ message: "please provide all the information" });
+  }
+  return res.status(201).json({
+    todo: [{ id: "todo_id", title, description, status: "done" }],
+  });
+});
 
-// list of the todo
-
-app.get("allTodo", (res,req)=>{
-
-    const {email}= req.body();
-
-    if(!email){
-        return res.send({
-            message : "please provide correct email id"
-        });
-    }
-
-    res.send({
-        todo: [
-            {
-                id : "1",
-                title: "gym",
-                description : "i have done for the two hour"
-            },
-            {
-                id : "2",
-                title: "playing",
-                description : "today, i play cricket "
-            }
-        ]
-    });
+// list todos - correct param order and accept email in query or body
+app.get("/todoList", (req, res) => {
+  const email = (req.query.email as string) || req.body?.email;
+  if (!email) {
+    return res.status(400).json({ message: "please provide correct email id" });
+  }
+  return res.status(200).json({
+    todo: [
+      { id: "1", title: "gym", description: "i have done for the two hour" },
+      { id: "2", title: "playing", description: "today, i play cricket" },
+    ],
+  });
 });
 
 app.listen(3000,()=>{
